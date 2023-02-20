976a82799740e2cbf78a6fa2cc90005534743147
tianhen <614891294@qq.com>
2023-02-20 16:00:40 +0800

    域名替换

--- src/config/proxy.ts
+++ src/config/proxy.ts
@@ -10,19 +10,19 @@ export default {
   isRequestProxy: true,
   development: {
     // 开发环境接口请求
-    host: 'https://yxxadminapi.fenxiangzl.com' || 'http://localhost:3002',
+    host: 'https://yxxadminapi.lechi.com' || 'http://localhost:3002',
     // 开发环境 cdn 路径
     cdn: '',
   },
   test: {
     // 测试环境接口地址
-    host: 'https://yxxadminapi.fenxiangzl.com',
+    host: 'https://yxxadminapi.lechi.com',
     // 测试环境 cdn 路径
     cdn: '',
   },
   release: {
     // 正式环境接口地址
-    host: 'https://yxxadminapi.fenxiangzl.com',
+    host: 'https://yxxadminapi.lechi.com',
     // 正式环境 cdn 路径
     cdn: '',
   },
