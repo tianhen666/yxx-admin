2bda221fe957f19653775914924dd828e51f8639
tianhen <614891294@qq.com>
2023-02-20 16:15:39 +0800

    域名修改

--- src/config/proxy.ts
+++ src/config/proxy.ts
@@ -10,19 +10,19 @@ export default {
   isRequestProxy: true,
   development: {
     // 开发环境接口请求
-    host: 'https://yxxadminapi.lechi.com' || 'http://localhost:3002',
+    host: 'https://yxxadminapi.lechiwl.com' || 'http://localhost:3002',
     // 开发环境 cdn 路径
     cdn: '',
   },
   test: {
     // 测试环境接口地址
-    host: 'https://yxxadminapi.lechi.com',
+    host: 'https://yxxadminapi.lechiwl.com',
     // 测试环境 cdn 路径
     cdn: '',
   },
   release: {
     // 正式环境接口地址
-    host: 'https://yxxadminapi.lechi.com',
+    host: 'https://yxxadminapi.lechiwl.com',
     // 正式环境 cdn 路径
     cdn: '',
   },
